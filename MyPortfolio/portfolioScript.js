132cd88dcb8a1ead1834626006d8dc1ef13bb85a
nomu <memento.mori2016@gmail.com>
2018-10-23 23:13:36 +0900

    fix scroll

--- MyPortfolio/portfolioScript.js
+++ MyPortfolio/portfolioScript.js
@@ -3,25 +3,25 @@ $(function() {
   $('.top').click(function() {
     $("html, body").animate({
       scrollTop:$('#top_area').offset().top
-    });
+    }, 800);
   });
 
   $('.about').click(function() {
     $("html, body").animate({
       scrollTop:$('#top-profile_area').offset().top
-    });
+    }, 800);
   });
 
   $('.works').click(function() {
     $("html, body").animate({
       scrollTop:$('#top-works_area').offset().top
-    });
+    }, 800);
   });
 
   $('.contact').click(function() {
     $("html, body").animate({
       scrollTop:$('#contact_area').offset().top
-    });
+    }, 800);
   });
 
   $('.top-works_item').click(function() {
